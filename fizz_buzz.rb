e9d1c924587e2a90c8b215e29f4178ba39a2b253
SayakaIw <160105syk@gmail.com>
2022-10-07 09:17:26 +0000

    fizzbuzzpractice

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(number)
-  if number%15==0
+  if number % 15 == 0
     "FizzBuzz"
-  elsif number%3==0
+  elsif number % 3 == 0
     "Fizz"
-  elsif number%5==0
+  elsif number % 5 == 0
     "Buzz"
   else
     number.to_s
@@ -12,5 +12,5 @@ end
 
 puts "数字を入力"
 input = gets.to_i
-puts "結果は。。"
+puts "結果は"
 puts fizz_buzz(input)
