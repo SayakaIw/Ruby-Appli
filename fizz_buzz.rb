c60fee90e2464e201eb3bda4f1007922eabde560
SayakaIw <160105syk@gmail.com>
2022-12-02 10:44:36 +0000

    fizzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(number)
-  if number % 15 ==0
-    "Fizzbuzz"
-  elsif number % 3 ==0
+  if number%15==0
+    "FizzBuzz"
+  elsif number%3==0
     "Fizz"
-  elsif number % 5 ==0
+  elsif number%5==0
     "Buzz"
   else
     number.to_s
