84123c82b11eb2f81eb8bfd0e0adaa55fd91da4b
SayakaIw <160105syk@gmail.com>
2022-12-21 09:18:42 +0000

    fizzbuzz

--- fizzbuzz4.rb
+++ fizzbuzz4.rb
@@ -1,10 +1,10 @@
-i = 1
+i=1
 while i <= 30
-  if i % 15==0
-    puts "FizzBuzz"
-  elsif i % 3==0
+  if i%15==0
+    puts "Fizzbuzz"
+  elsif i%3==0
     puts "Fizz"
-  elsif i % 5==0
+  elsif i%5==0
     puts "Buzz"
   else
     puts i
